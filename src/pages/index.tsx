54158e0b1a88934b35ee4b4adaf1c6958466a71c
Sami <melahi@uchicago.edu>
2023-03-21 08:47:33 -0700

    yoo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Logo from "~/components/Logo";
+import Image from "next/image";
 import SearchBar from "~/components/SearchBar";
 
 const Home: NextPage = () => {
@@ -11,10 +12,19 @@ const Home: NextPage = () => {
         <meta name="description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex items-center justify-center">
-        <div className="mt-20 flex w-fit flex-col items-center gap-8">
+      <main className="flex h-screen flex-col items-center justify-between">
+        <div className="mt-28 flex w-fit flex-col items-center gap-8">
           <Logo size="large" />
           <SearchBar />
+        </div>
+        <div className="relative h-[300px] md:h-[650px] w-full overflow-hidden">
+          <Image
+            quality={100}
+            alt=""
+            src={"/tickets.png"}
+            fill={true}
+            className="object-cover object-top md:mt-20"
+          ></Image>
         </div>
       </main>
     </>
