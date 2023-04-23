a24532e4cfec57c5ed71afb8c4a31aa4a13efc11
Sami <melahi@uchicago.edu>
2023-04-23 15:59:02 -0700

    edit section done

--- src/components/Archiver/EditSection/EditSection.tsx
+++ src/components/Archiver/EditSection/EditSection.tsx
@@ -1,12 +1,18 @@
-import { useArchiver } from "../ArchiverContext";
+import { useArchiver, useArchiverDispatch } from "../ArchiverContext";
 import SectionWrapper from "../SectionWrapper";
 import RowEditor from "./RowEditor";
+import NextButton from "../NextButton";
 import { Section } from "../types";
-import type { Row } from "../types";
 
 export default function EditSection() {
   const { currentSection, rows } = useArchiver()!;
-  const hasErrors = (row: Row) => Object.keys(row.errors!).length > 0;
+  const dispatch = useArchiverDispatch()!;
+  const rowsWithErrors = rows.filter(
+    (row) => Object.keys(row.errors!).length > 0
+  );
+
+  console.log(rowsWithErrors);
+
   return (
     <>
       {currentSection === Section.edit && (
@@ -15,14 +21,27 @@ export default function EditSection() {
           description="If there are any issues detected with the uploaded data, you can fix them here."
           section={currentSection}
         >
-          <div className="mt-6 md:mt-10">
-            <h2 className="font-bold">Issues Found</h2>
-            {rows?.map((row, i) => {
-              if (hasErrors(row)) {
-                return <RowEditor key={i} rowId={i} rowWithError={row} />;
-              }
-            })}
-          </div>
+          {rowsWithErrors.length ? (
+            <div className="mt-6 md:mt-10">
+              <h2 className="font-bold">Issues Found</h2>
+              {rows?.map((row, i) => {
+                if (Object.keys(row.errors!).length > 0) {
+                  return <RowEditor key={i} rowId={i} rowWithError={row} />;
+                }
+              })}
+            </div>
+          ) : (
+            <div className="mt-6 flex w-full items-center justify-between md:mt-10">
+              <p>No issues found!</p>
+              <NextButton
+                onClick={() => {
+                  dispatch({
+                    type: "ADVANCE",
+                  });
+                }}
+              />
+            </div>
+          )}
         </SectionWrapper>
       )}
     </>
